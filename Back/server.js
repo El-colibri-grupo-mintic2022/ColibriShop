826bb2952c9e76da4378d27994be9344eded83c2
Albertosb10 <albertosb10@outlook.com>
2022-10-06 22:06:30 -0500

    package.json configuration

--- Back/server.js
+++ Back/server.js
@@ -1,1 +1,11 @@
-const app = require("app")
+const app = require("./app")
+
+//seting configuration file 
+const dotenv = require("dotenv");
+dotenv.config({path: 'Back/config/config.env'})
+
+
+//server
+const server = app.listen(process.env.PORT, () => {
+    console.log(`Servidor iniciado en el puerto: ${process.env.PORT} en modo: ${process.env.NODE_ENV}`)
+})
