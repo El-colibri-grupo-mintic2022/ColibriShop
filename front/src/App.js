89ad013c5e22b6026e630a48010887942224978a
Albertosb10 <albertosb10@outlook.com>
2022-10-24 11:44:29 -0500

    front preliminar

---
+++ front/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Sebastiano Bake Shop</h1>
+        <img src=".\images\sebastiano bake shop logo (1).png" alt='Aqui va su logo'></img>
+    </div>
+  );
+}
+
+export default App;
